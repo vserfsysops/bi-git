d1444c02f6b7ad77d9c5d862dee9a1fe19fa6a84
Om Shankar <oshankar@linkedin.com>
2016-05-23 22:45:08 -0700

    Slider changes to load images from imgur

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,19 @@
+---
+---
 var map;
 $(document).ready(function() {
 
-    /* ======= Flexslider ======= */
-    $('.flexslider').flexslider({
-        animation: "fade"
-    });
-
     /* ======= jQuery Placeholder ======= */
     $('input, textarea').placeholder();
-    
-    
+
+
     /* ======= Carousels ======= */
     $('#news-carousel').carousel({interval: false});
     $('#videos-carousel').carousel({interval: false});
     $('#testimonials-carousel').carousel({interval: 6000, pause: "hover"});
     $('#awards-carousel').carousel({interval: false});
-    
-    
+
+
     /* ======= Flickr PhotoStream ======= */
     $('#flickr-photos').jflickrfeed({
         limit: 12,
@@ -29,65 +26,101 @@ $(document).ready(function() {
             	'<img src="{{image_s}}" alt="{{title}}" />' +
             '</a>' +
             '</li>'
-			
+
     }, function(data) {
     	$('#flickr-photos a').prettyPhoto();
     });
-    
+
     /* ======= Pretty Photo ======= */
-    // http://www.no-margin-for-errors.com/projects/prettyphoto-jquery-lightbox-clone/ 
+    // http://www.no-margin-for-errors.com/projects/prettyphoto-jquery-lightbox-clone/
     $('a.prettyphoto').prettyPhoto();
-    
+
     /* ======= Twitter Bootstrap hover dropdown ======= */
-    
+
     // apply dropdownHover to all elements with the data-hover="dropdown" attribute
     $('[data-hover="dropdown"]').dropdownHover();
-    
+
     /* Nested Sub-Menus mobile fix */
-    
+
     $('li.dropdown-submenu > a.trigger').on('click', function(e) {
         var current=$(this).next();
 		current.toggle();
-		e.stopPropagation(); 
-		e.preventDefault(); 
+		e.stopPropagation();
+		e.preventDefault();
 		if (current.is(':visible')) {
     		$(this).closest('li.dropdown-submenu').siblings().find('ul.dropdown-menu').hide();
 		}
-    });     
-    
-    
+    });
+
+
     /* ======= Style Switcher ======= */
-    
+
     $('#config-trigger').on('click', function(e) {
         var $panel = $('#config-panel');
         var panelVisible = $('#config-panel').is(':visible');
         if (panelVisible) {
-            $panel.hide();        
+            $panel.hide();
         } else {
             $panel.show();
         }
         e.preventDefault();
     });
-    
+
     $('#config-close').on('click', function(e) {
         e.preventDefault();
         $('#config-panel').hide();
     });
-    
-    
-    $('#color-options a').on('click', function(e) { 
+
+
+    $('#color-options a').on('click', function(e) {
         var $styleSheet = $(this).attr('data-style');
         var $logoImage = $(this).attr('data-logo');
 		$('#theme-style').attr('href', $styleSheet);
-		$('#logo').attr('src', $logoImage);		
-				
+		$('#logo').attr('src', $logoImage);
+
 		var $listItem = $(this).closest('li');
 		$listItem.addClass('active');
 		$listItem.siblings().removeClass('active');
-		
+
 		e.preventDefault();
-		
+
 	});
 
+    $.ajax({
+        type: "GET",
+        url: "https://api.imgur.com/3/album/{{ site.imgur.tempAlbumId }}/images",
+        dataType: "text",
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader('Authorization', 'Client-ID {{ site.imgur.clientId }}');
+        },
+        success: function(data) {
+            var json = $.parseJSON(data);
+
+            json.data.sort(function(a, b) {
+                return a.datetime > b.datetime;
+            });
+            var slidesList = $('<ul></ul>');
+
+            for (var i = 0; i < 5; i++) {
+                var image = json.data[i];
+                var liHtml = ''+
+                    '<img src="' + image.link + '"  alt="' + image.link + '" />' +
+                    '<p class="flex-caption">' +
+                    '  <span class="main" >' + image.title + '</span>' +
+                    '  <br />' +
+                    '  <span class="secondary clearfix" >' + (image.description ? image.description : '') + '</span>' +
+                    '</p>';
+                $(document.createElement('li')).append(liHtml).appendTo(slidesList);
+            }
+
+            $('#promo-slider').append(slidesList);
+            slidesList.addClass('slides');
+
+            /* ======= Flexslider ======= */
+            $('.flexslider').flexslider({
+                animation: "fade"
+            });
+        }
+    });
 
 });
